dd4d373d1d5b5faea66e4568ec67b0811711bab7
Drillu <maxmax98765@yahoo.ro>
2021-05-31 19:04:16 +0300

    MainMenuChanges

--- AI-and_Games-UnityProject/Assets/scripts/UI/MainMenuScript.cs
+++ AI-and_Games-UnityProject/Assets/scripts/UI/MainMenuScript.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Animator transition;
+    public float transTime;
+
+    IEnumerator LoadLevel(int indexLevel)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transTime);
+        SceneManager.LoadScene(indexLevel);
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
     public void QuitGame()
     {
         Application.Quit();
