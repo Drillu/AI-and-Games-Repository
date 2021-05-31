d399b051d45e3fae1921f6e204d8dd48dd523e3d
petifox <73014276+petifox@users.noreply.github.com>
2021-05-31 16:35:40 +0200

    thing

--- AI-and_Games-UnityProject/Assets/scripts/PlayerMove.cs
+++ AI-and_Games-UnityProject/Assets/scripts/PlayerMove.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMove : MonoBehaviour
 {
     private NavMeshAgent agent;
