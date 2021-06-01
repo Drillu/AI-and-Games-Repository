2351872d7bec108012503efc008325c47c9c2a7a
petifox <73014276+petifox@users.noreply.github.com>
2021-06-01 22:44:22 +0200

    loot

---
+++ AI-and_Games-UnityProject/Assets/scripts/inventory/Loot.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    public List<LootableItem> items;
+    public Inventory inventory;
+    public bool destroyOnLoot;
+    public float radius;
+    public LayerMask PlayerLayer;
+    private bool looted;
+    
+    public void onLoot()
+    {
+        if(looted == false)
+        {
+            foreach (LootableItem i in items)
+            {
+                if(Chance(i.probability))
+                {
+                    inventory.AddItem(i.item, i.amount);
+                }
+            }   
+        }
+        looted = true;
+        if(destroyOnLoot)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private bool Chance(float probability)
+    {
+        if(Random.Range(0, 100) < probability)
+        {
+            return true;
+        }
+        return false;
+    }
+    private void Update()
+    {
+        Collider[] player = Physics.OverlapSphere(transform.position, radius, PlayerLayer);
+        if (player.Length != 0)
+        {
+            onLoot();
+        }
+    }
+}
---
+++ AI-and_Games-UnityProject/Assets/scripts/inventory/LootableItem.cs
@@ -0,0 +1,8 @@
+
+[System.Serializable]
+public class LootableItem
+{
+    public Item item;
+    public int amount;
+    public float probability;
+}
