02d305a1f44357cb87a161f252764feb9e0da7fb
jinming-zhang <jinming.zhang47@gmail.com>
2021-06-09 04:10:09 -0400

    working on dialogue

--- AI-and_Games-UnityProject/Assets/UI/Scripts/DialoguePanel.cs
+++ AI-and_Games-UnityProject/Assets/UI/Scripts/DialoguePanel.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DialoguePanel : MonoBehaviour
 {
-	public void Initialize()
-	{
+    [SerializeField] Image icon;
+    [SerializeField] TMPro.TextMeshProUGUI characterName;
+    [SerializeField] TMPro.TextMeshProUGUI dialogueText;
 
-	}
+    Coroutine setTextCR;
+    public void Initialize()
+    {
+
+    }
+
+    public void SetDialogue(Sprite iconSprite, string charName, string text, bool animated = true)
+    {
+        if (animated)
+        {
+            setTextCR = StartCoroutine(SetDialogueCR(iconSprite, charName, text));
+        }
+        else
+        {
+            SetDialogueQuick(iconSprite, charName, text);
+        }
+    }
+
+    private void SetDialogueQuick(Sprite iconSprite, string charName, string text)
+    {
+        icon.sprite = iconSprite;
+        characterName.text = charName;
+        dialogueText.text = text;
+    }
+
+    private IEnumerator SetDialogueCR(Sprite iconSprite, string charName, string text)
+    {
+        icon.sprite = iconSprite;
+        characterName.text = charName;
+        dialogueText.text = text;
+        yield return new WaitForEndOfFrame();
+    }
+	
 }
