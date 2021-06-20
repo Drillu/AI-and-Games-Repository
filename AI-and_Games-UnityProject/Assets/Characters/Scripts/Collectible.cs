11fefec5a856b51382f162f04cf1cbd56d912825
Jinming-Zhang <jinming.zhang47@gmail.com>
2021-06-20 04:46:11 -0400

    added some particles

--- AI-and_Games-UnityProject/Assets/Characters/Scripts/Collectible.cs
+++ AI-and_Games-UnityProject/Assets/Characters/Scripts/Collectible.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Collectible : MonoBehaviour
+using Inventorys;
+public class Collectible : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public InventoryItem collectibleItem;
+    [SerializeField] public float interactRange;
+	public float GetInteractRange()
+	{
+        return interactRange;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Vector3 GetPosition()
+	{
+        return transform.position;
+	}
+
+	public void Interact(GameObject initiater)
+	{
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireSphere(transform.position, interactRange);
+	}
 }
