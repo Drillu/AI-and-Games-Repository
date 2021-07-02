b688a3eacb912f1fc8746fdc38a2bdab539ae477
Jinming-Zhang <jinming.zhang47@gmail.com>
2021-07-02 18:12:37 -0400

    new backup before converting tables

--- AI-and_Games-UnityProject/Assets/ComponentConverter.cs
+++ AI-and_Games-UnityProject/Assets/ComponentConverter.cs
@@ -7,13 +7,13 @@ public class ComponentConverter : MonoBehaviour
 	[MenuItem("Converter/Convert Kitchen Table")]
 	public static void ConvertTable()
 	{
-		GameObject kitchen = GameObject.Find("Kitchen");
+		GameObject kitchen = GameObject.Find("Kitchenn");
 		GameObject newTable = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/GamePlay/SceneObjects/Table.prefab");
 		foreach (GameObject table in kitchen.transform)
 		{
 			if (table.name.StartsWith("Table"))
 			{
-				GameObject ntable = Instantiate(newTable, kitchen.transform);
+				GameObject ntable = (GameObject)PrefabUtility.InstantiatePrefab(newTable, kitchen.transform);
 				ntable.transform.position = table.transform.position;
 				ntable.transform.rotation = table.transform.rotation;
 				DestroyImmediate(table);
